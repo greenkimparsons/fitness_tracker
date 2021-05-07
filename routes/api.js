6ecb4b791f6b682095c6c6a318ef06a65c40a856
Green Kim <greenkimparsons@gmail.com>
2021-05-07 17:21:08 -0400

    worked with tutor for route edits

--- routes/api.js
+++ routes/api.js
@@ -1,48 +1,63 @@
 const router = require("express").Router();
 const db = require("../models");
 
-// add exercises to most recent workout plan
-router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.put("/api/workouts/:id", ( req,res) => {
-  db.Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body }}, {new:true})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-// add new exercises to new workout plan
+// get all workouts
 router.get("/api/workouts", (req, res) => {
-  Transaction.find({})
+  db.Workout.find({})
     .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/workouts/range", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+// add new exercise to workout plan
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
+
+// add exercise to most recent workout plan
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+//view combined weight of multiple exercises from the past 7 workouts
+//view total duration of each workout from past 7 workouts
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .limit(5)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+//aggregate the total duration
+// limit the number of results
 
 module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const path = require("path");
 
+//views
 router.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
